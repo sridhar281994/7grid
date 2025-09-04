5ce01346d7323da726dd40b2c74d5e002b018824
sridhar281994 <sri.christh@gmail.com>
2025-09-05 01:42:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import RedirectResponse
 from database import Base, engine
 from routers import auth, users, wallet, game, match_routes
+from routers import ws_routes
+
 
 app = FastAPI(title="Spin Dice API", version="1.0.0")
 
@@ -32,3 +34,4 @@ app.include_router(users.router)
 app.include_router(wallet.router)
 app.include_router(game.router)
 app.include_router(match_routes.router)
+app.include_router(ws_routes.router)

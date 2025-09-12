618716de9c95b079c6ab0660752f696c7826b6da
sridhar281994 <sri.christh@gmail.com>
2025-09-13 02:49:50 +0530

    Create redis_client.py

---
+++ utils/redis_client.py
@@ -0,0 +1,10 @@
+import os
+import redis.asyncio as redis
+
+REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
+
+redis_client = redis.from_url(
+    REDIS_URL,
+    decode_responses=True,
+    encoding="utf-8",
+)
